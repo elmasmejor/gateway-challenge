eb8c7d8349eff5cbbaa2d8fda13281532640e458
Leo <leo.barban@isucorp.ca>
2023-05-11 00:35:42 -0400

    - create new scaffold: exec nest new gateways-api - add and config swagger: pnpm add @nestjs/swagger

---
+++ gateways-api/src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Gateways API')
+    .setDescription('manage gateways and devices')
+    .setVersion('1.0')
+    .addTag('management')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  const port = process.env.APP_PORT || 3000;
+  await app.listen(port);
+  console.log('running on port: ' + port);
+}
+
+bootstrap();
