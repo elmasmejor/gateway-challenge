18d4b391d57774032138f9fa91ccb4669cbad363
Leo <leo.barban@isucorp.ca>
2023-05-11 01:34:15 -0400

    - add mongoose missing config change

--- gateways-api/src/app.module.ts
+++ gateways-api/src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GatewayModule } from './gateway/gateway.module';
 import { DeviceModule } from './device/device.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('CONFIG_MONGODB_URL'),
+      }),
     }),
     GatewayModule,
     DeviceModule,
